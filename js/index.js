65cce062e71c61b3a24a81e8113cc4a3d98ec064
Agony-C <2682852349@qq.com>
2021-04-01 23:01:09 +0800

    上传文件

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var show=document.getElementById('show');
+var tanc=document.getElementById('tanc');
+var guan=document.getElementById('guan');
+    show.onclick=function(){
+        tanc.style.display = "block";
+ }
+    guan.onclick=function(){
+        tanc.style.display = "none";
+ }
+
